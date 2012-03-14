6fe5cc06fabc3912bae759380e3bf9133f3b2177
Tom Swiggers <swiggers.tom@gmail.com>
2012-03-14 11:32:17 +0100

    add color for cli mode

--- test/show_passes.php
+++ test/show_passes.php
@@ -18,25 +18,37 @@ class ShowPassesHtml extends HtmlReporter {
   }
 }
 
-class ShowPassesText extends TextReporter {
-      
+class ShowPassesText extends TextReporter 
+{
+  public $red = "\033[31;40m\033[1m%s\033[0m";
+  public $green = "\033[1;32;40m\033[1m%s\033[0m";
+
   function paintPass($message) 
   {
     parent::paintPass($message);
-    echo 'Pass: ';
+    printf($this->green, 'Pass');
     $breadcrumb = $this->getTestList();
     array_shift($breadcrumb);
     
-    print implode('->', $breadcrumb);
-    print '->'. $message .PHP_EOL;
+    echo ' '. implode('->', $breadcrumb);
+    echo '->'. $message .PHP_EOL;
   }
   
   function paintFooter($test_name) 
   {
-    print "Test cases run: " . $this->getTestCaseProgress() .
+    $str = "Test cases run: " . $this->getTestCaseProgress() .
       "/" . $this->getTestCaseCount() .
       ", Passes: " . $this->getPassCount() .
       ", Failures: " . $this->getFailCount() .
-      ", Exceptions: " . $this->getExceptionCount() . "\n";
+      ", Exceptions: " . $this->getExceptionCount();
+
+    if ($this->getFailCount() + $this->getExceptionCount() > 0) {
+      $color = $this->red;
+    } else {
+      $color = $this->green;
+    }
+
+    printf($color, $str);
+    echo PHP_EOL;
   }  
 }
